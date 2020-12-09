8930616b7739e56630bab3259cf59bd9be6599a2
Willian Motta Ferreira Braga <willianmfb@gmail.com>
2020-12-09 00:02:40 -0300

    aula1
    primeiros códigos da aula introdutoria de Js

---
+++ aulasDio/main.js
@@ -0,0 +1,60 @@
+var fruta = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);
+
+
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+
+
+
+
+
+//var lista = ["Maçã","pêra","laranja"];
+
+//lista.push("uva"); Usado para adicionar mais itens.
+
+//lista.pop(); Usado para tirar o ultimo elemento da lista neste exemplo saira a Laranja.
+
+//console.log(lista.length); Mostra a quandidade de itens que tem na lista.
+
+//console.log(lista.reverse()); imprimi os itens da lista na tela de trás pra frente.
+
+//console.log(lista.toString()); coloca na tela de array para string.
+
+//console.log(lista.join("-")); coloca na tela de array para string, mas você consegue colocar qualquer simbolo.
+
+//console.log(lista);
+
+
+
+
+
+
+
+/*var nome = "Willian";
+var n1 = 31;
+var n2 = 10;
+
+var frase = "Japão e o melhor time do mundo";
+
+console.log(nome);
+console.log(n1*n2);
+console.log(frase.toLowerCase());*/
+
+
+//console.log(frase.toLowerCase()); Coloca todas as letras em minusculas.
+
+//console.log(frase.toUpperCase()); Coloca todas as letras em Maiusculas.
+
+//alert(frase.replace("Japão","Brasil"));
+
+
+//alert(idade+idade2) soma as duas idades
+
+//alert(nome + " tem " + idade + " anos.")
+
+//alert("Meu primeiro Js") aparece um pop up assim que abre a página.
