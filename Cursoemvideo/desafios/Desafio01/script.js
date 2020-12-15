cc8d86ca37ca8de1395486ad05c69830adf0ea87
Willian Motta Ferreira Braga <willianmfb@gmail.com>
2020-12-15 00:35:55 -0300

    continuação do curso e desafio 1

---
+++ Cursoemvideo/desafios/Desafio01/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >= 0 && hora < 12){
+    //Bom dia
+    img.src = "manha.png" 
+    document.body.style.background = '#ffe764'
+} else if( hora >= 12 && hora<= 18){
+    //Boa tarde
+    img.src = "Tarde.png"
+    document.body.style.background = '#a54309'
+}else{
+    //Boa noite
+    img.src = "noite.png"
+    document.body.style.background = '#083842'
+}
+}
