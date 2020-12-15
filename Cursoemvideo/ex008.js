c43db43e77794fa7800aeafad1633cd982748e13
Willian Motta Ferreira Braga <willianmfb@gmail.com>
2020-12-14 22:50:36 -0300

    continuação do curso

---
+++ Cursoemvideo/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){ //condição simples, if significa 'se'.
+    console.log("Você ultrapassou a velocidade permitida.Multado!")
+}
+console.log("Dirija sempre usando o sinto de segurança!")
