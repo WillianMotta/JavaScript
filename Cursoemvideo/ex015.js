8fa65795e6af120338ffffa2eb952377e2265f13
Willian Motta Ferreira Braga <willianmfb@gmail.com>
2020-12-16 00:23:39 -0300

    continuação

---
+++ Cursoemvideo/ex015.js
@@ -0,0 +1,17 @@
+// Estrutura Basica de Repetição.
+
+/* var c = 1 //while = Enquanto
+while( c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*var c = 1 // do = faça
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while( c <= 6)*/
+
+for(var c = 1;c <= 5; c++){
+    console.log(c)
+}
